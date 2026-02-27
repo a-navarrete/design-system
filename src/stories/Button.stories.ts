2382bfb73ff8284d179b433ae6937a23d8906caf
a-navarrete <hello@arturonavarrete.co>
2026-02-27 16:06:01 -0500

    Fix Storybook build: resolve duplicate keys and MDX parsing errors

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -10,8 +10,6 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
-  },
-  parameters: {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/design/iJ8gvICLXnbsh9ple9rogK/Design-Systems?node-id=2-5&m=dev',
